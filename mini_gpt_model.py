91b059c22430a27e26a67160b55f95591a748032
lucaschia800 <lucaschia800@gmail.com>
2025-04-03 12:21:41 -0700

    upating

--- mini_gpt_model.py
+++ mini_gpt_model.py
@@ -128,8 +128,6 @@ class GPT(nn.Module):
                 # GPT-2 configs
                 'gpt2':         dict(n_layer=12, n_head=12, n_embd=768),  # 124M params
                 'gpt2-medium':  dict(n_layer=24, n_head=16, n_embd=1024), # 350M params
-                'gpt2-large':   dict(n_layer=36, n_head=20, n_embd=1280), # 774M params
-                'gpt2-xl':      dict(n_layer=48, n_head=25, n_embd=1600), # 1558M params
                 # Gophers
                 'gopher-44m':   dict(n_layer=8, n_head=16, n_embd=512),
                 # (there are a number more...)
